6442066263d78b2143368d27fcc70f09e3c6e0ce
Evangeline Drink <evangelinedrink@gmail.com>
2021-08-23 15:59:13 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace Time_Management_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Run the todaysTemp method (Weather App Component of the Time Management Application)
+            todaysTemp();
+        }
+
+        //Pre-conditions: The input placed by the user has to be converted into integers to be passed through the DateTime method. The variable from the DateTime method will then be called userDate.
+        //userDate has to be a string that says what the day of the week is. userDate will be pased into the todaysTemp method.
+
+        public static void todaysTemp()
+        {
+            //Ask the user what the year is (user input will be determined by the Console.ReadLine())
+            Console.WriteLine("What is the year?");
+            //User will type today's date in the Console and Console will read what is typed
+            string userYear = Console.ReadLine();
+
+            //Edge Statement: If the user places a year that is below 2020 and above 2030, an error message will be shown
+            while ((Convert.ToInt32(userYear) <= 2020) || (Convert.ToInt32(userYear) >= 2030)) //Loop will continue if these conditions are met
+            {
+                //Error Message saying they have to enter a year between 2020 and 2030
+                Console.WriteLine("The year you enter has to be between 2020 and 2030. Please enter the correct year.");
+                //Ask the user what the year is (user input will be determined by the Console.ReadLine())
+                Console.WriteLine("What is the year?");
+                //User will type today's date in the Console and Console will read what is typed
+                userYear = Console.ReadLine();
+            } 
+
+
+            //Ask the user what month the date is in (user input will be determined by the Console.ReadLine())
+            Console.WriteLine("What is the month? Please write it in number form. If it is May, you would type 5.");
+            //User will type today's date in the Console and Console will read what is typed
+            string userMonth = Console.ReadLine();
+
+            //Edge Statement: If the user places a month that is below 1 and above 12, an error message will be shown
+            while ((Convert.ToInt32(userMonth) < 1) || (Convert.ToInt32(userMonth) > 12)) //If the user types in a month below 1 or a month above 12, the statement in the do loop will continue to run.
+            {
+                //Error Message that tells the user that they have to enter a month between 1 and 12
+                Console.WriteLine("The month you enter has to be between 1 (which corresponds to January) and 12 (corresponds to December). Please enter the correct number corresponding to its correct month.");
+                //Ask the user what month the date is in (user input will be determined by the Console.ReadLine())
+                Console.WriteLine("What is the month? Please write it in number form. If it is May, you would type 5.");
+                //User will type today's date in the Console and Console will read what is typed
+                userMonth = Console.ReadLine();
+            } 
+
+
+            //Ask the user what the day is (user input will be determined by the Console.ReadLine())
+            Console.WriteLine("What is the day? Please type the number of the day.");
+            //User will type today's date in the Console and Console will read what is typed
+            string userDay = Console.ReadLine();
+
+            //Edge Statement: Depending on the month and the leap years, the number of days in each month will be different. If the user types in a day that is not within that month, an error will appear
+            //Months that have 31 days in them are placed in an array (January, March, May, July, August, October and December) 
+            //int[] months31Days = { 1, 3, 5, 7, 8, 10, 12 };
+
+            //Month converted to an integer data type
+            int userMonthInt = Convert.ToInt32(userMonth);
+
+            //Using and If/Else If loop to go through the unacceptable days in certain months
+            //Months that have 31 days in them are placed in an array (January, March, May, July, August, October and December) 
+            if ((userMonthInt==1) || (userMonthInt == 3) || (userMonthInt == 5) || (userMonthInt == 7)
+                || (userMonthInt == 8) || (userMonthInt == 10) || (userMonthInt == 12))
+            {
+                //As long as the conditions for the While loop are true, the code inside of the While Loop's body will continue to run.
+                while((Convert.ToInt32(userDay)<1) || (Convert.ToInt32(userDay) > 31))
+                {
+                    //Error Message will appear in the Console if the user types in a day that is below 1 and over 31
+                    Console.WriteLine("For the month that you typed in, it has days between 1 and 31. Please type in the correct day.");
+                    //Ask the user what the day is (user input will be determined by the Console.ReadLine())
+                    Console.WriteLine("What is the day? Please type the number of the day.");
+                    //User will type today's date in the Console and Console will read what is typed
+                    userDay = Console.ReadLine();
+                }
+            } else if ((userMonthInt == 4) || (userMonthInt == 6) || (userMonthInt == 9) || (userMonthInt == 11))  //Months that have 30 days in them
+            {
+                //As long as the conditions for the While loop are true, the code inside of the While Loop's body will continue to run.
+                while ((Convert.ToInt32(userDay) < 1) || (Convert.ToInt32(userDay) > 30))
+                {
+                    //Error Message will appear in the Console if the user types in a day that is below 1 and over 31
+                    Console.WriteLine("For the month that you typed in, it has days between 1 and 30. Please type in the correct day.");
+                    //Ask the user what the day is (user input will be determined by the Console.ReadLine())
+                    Console.WriteLine("What is the day? Please type the number of the day.");
+                    //User will type today's date in the Console and Console will read what is typed
+                    userDay = Console.ReadLine();
+                }
+            } else if ((userMonthInt==2) && ((Convert.ToInt32(userYear)==2020) || (Convert.ToInt32(userYear) == 2024) || (Convert.ToInt32(userYear) == 2028))) //February has variable days depending if it is a leap year or not. During a leap year, February has 29 days, not during a leap year it has 28 days.
+            {
+                //The body of this Else If statement will run for leap years, meaning that February will have 29 days. While statement will be used to show an error if the user types in a day below 1 or over 29
+                while ((Convert.ToInt32(userDay) < 1) || (Convert.ToInt32(userDay) > 29))
+                {
+                    //Error Message will appear in the Console if the user types in a day that is below 1 and over 31
+                    Console.WriteLine("The year that you typed in is a Leap Year. This means that February has days between 1 and 29. Please type in the correct day.");
+                    //Ask the user what the day is (user input will be determined by the Console.ReadLine())
+                    Console.WriteLine("What is the day? Please type the number of the day.");
+                    //User will type today's date in the Console and Console will read what is typed
+                    userDay = Console.ReadLine();
+                }
+            }
+            else //The body of this else statement will run when it is not a leap year and the month is February.
+            {
+                //There are only 28 days in February when it is not a leap year. If the user types in a date below 1 and greater than 28, an error message will appear in the Console.
+                //While statement will check to ensure that the correct date in February when it is not a leap year has been typed, if not an error message and asking the user to retype the date will appear
+                //The body of this Else If statement will run for leap years, meaning that February will have 29 days. While statement will be used to show an error if the user types in a day below 1 or over 29
+                while ((Convert.ToInt32(userDay) < 1) || (Convert.ToInt32(userDay) > 28))
+                {
+                    //Error Message will appear in the Console if the user types in a day that is below 1 and over 31
+                    Console.WriteLine("The year that you typed in is not a Leap Year. This means that February has days between 1 and 28. Please type in the correct day.");
+                    //Ask the user what the day is (user input will be determined by the Console.ReadLine())
+                    Console.WriteLine("What is the day? Please type the number of the day.");
+                    //User will type today's date in the Console and Console will read what is typed
+                    userDay = Console.ReadLine();
+                }
+            }
+
+            //Determine the day of the week for the date typed by the user
+            //DateTime will get the date and the time (in this case it is specified by the user)
+            //DateTime syntax goes by year, month and day
+            DateTime dateValue = new DateTime(Convert.ToInt32(userYear), Convert.ToInt32(userMonth), Convert.ToInt32(userDay));
+            string userDate = dateValue.ToString("dddd"); //dateValue will determine the full weekday name if the parameter is "dddd". ToString() method will make it a string (before it was an integer)
+
+
+            //Display what the user has written in the Console
+            Console.WriteLine("Today's date is: " + userDate);
+
+
+            //Temperatures for that area during the week
+            //Since an API is not used, static temperatures in an array will contain the temperatures for all days of the week (from Monday through Sunday)
+            double[] weekTemps = { 78.2, 80.4, 82.5, 77.6, 73.2, 71.7, 82.3 };
+
+            //Declaring the temperature for that date. This value will be changed depending on the day of the week it is.
+            double todayTemp = 0.0;
+
+            //Determining the temperature based on the day of the week. If/Else If statements are used to determine what the temperature is based on the day of the week.
+            if (userDate == "Monday")
+            {
+                todayTemp = weekTemps[0];
+            }
+            else if (userDate == "Tuesday")
+            {
+                todayTemp = weekTemps[1];
+            }
+            else if (userDate == "Wednesday")
+            {
+                todayTemp = weekTemps[2];
+            }
+            else if (userDate == "Thursday")
+            {
+                todayTemp = weekTemps[3];
+            }
+            else if (userDate == "Friday")
+            {
+                todayTemp = weekTemps[4];
+            }
+            else if (userDate == "Saturday")
+            {
+                todayTemp = weekTemps[5];
+            }
+            else if (userDate == "Sunday")
+            {
+                todayTemp = weekTemps[6];
+            }
+
+            //Displaying the temperature to the user on the Console. Template literal is used to make it easier to write the code.
+            Console.WriteLine($"The temperature for {userDate} is: {todayTemp} degrees F.");
+        }
+
+    }
+}
